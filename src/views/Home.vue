cf409d3d148142319564e3cb9c919074aa72f452
EastHill <aquabuz@gmail.com>
2022-02-21 22:04:33 +0900

    leaflet mounted

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="flex flex-col h-screen max-h-screen">
+    <!-- Search / Results -->
     <div
-      class="flex justify-center relative bg-blue-200 bg-cover px-4 pt-8 pb-32"
+      class="flex justify-center relative bg-blue-200 bg-cover px-4 pt-8 pb-32 z-20"
     >
+      <!-- Search Input -->
       <div class="w-full max-w-screen-sm">
         <h1 class="text-blue-400 text-center text-3xl pb-4">
           Ip Address Tracker
@@ -18,17 +20,47 @@
           ></i>
         </div>
       </div>
+      <!-- IP Info -->
       <IPInfo />
     </div>
+    <!-- Map -->
+    <div id="map" class="h-full z-10"></div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import IPInfo from "@/components/IPInfo.vue";
+import { map, tileLayer } from "leaflet";
+import { onBeforeUnmount, onMounted } from "vue";
 
 export default {
   name: "Home",
   components: { IPInfo },
+  setup() {
+    let mymap;
+
+    onMounted(() => {
+      mymap = map("map").setView([51.505, -0.09], 13);
+
+      tileLayer(
+        "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYXF1YWJ1eiIsImEiOiJja3p3b3U5MjEyam9yMnBuYWZrZHpybnB2In0.q3DIjcD4FGSpi990q97W4A",
+        {
+          attribution:
+            'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+          maxZoom: 18,
+          id: "mapbox/streets-v11",
+          tileSize: 512,
+          zoomOffset: -1,
+          accessToken:
+            "pk.eyJ1IjoiYXF1YWJ1eiIsImEiOiJja3p3b3U5MjEyam9yMnBuYWZrZHpybnB2In0.q3DIjcD4FGSpi990q97W4A",
+        }
+      ).addTo(mymap);
+    });
+
+    onBeforeUnmount(() => {
+      mymap.remove();
+    });
+  },
 };
 </script>
