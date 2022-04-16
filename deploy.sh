78ed7b12c4b4d1e5c54215b9f5f3dfea23f793a0
aquabuz <aquabuz@gmail.com>
2022-04-17 04:28:22 +0900

    😎 FIN+

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/aquabuz/IPADDRESSTRACKER master:gh-pages
+git push -f https://github.com/aquabuz/IPADDRESSTRACKER.git master:gh-pages
 
 cd -
